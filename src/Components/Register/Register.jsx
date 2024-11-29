1ae7071dc1dd62d02b200d05f76177d31f31754f
Mazharul Islam <mazharul3islam0@gmail.com>
2024-11-30 01:32:53 +0600

    Email password Authentication project setup

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -2,7 +2,16 @@
 const Register = () => {
     return (
         <div>
-            <h2 className="text-3xl">Please Register</h2>
+            <div className="mx-auto md:n w-1/2">
+                <h2 className="text-3xl mb-8">Please Register</h2>
+                <form>
+                    <input className="mb-4 w-3/4 py-2 px-4" type="email" placeholder="Email Address" name="email" id="" />
+                    <br />
+                    <input className="mb-4 w-3/4 py-2 px-4" type="password" name="password" placeholder="Password" id="" />
+                    <br />
+                    <input className="btn btn-secondary mb-4 w-3/4" type="submit" value="Register" />
+                </form>
+            </div>
         </div>
     );
 };
