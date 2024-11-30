8593c28a95db613c2a1151a6c0fe8d71744e37cf
Mazharul Islam <mazharul3islam0@gmail.com>
2024-11-30 20:07:46 +0600

    push

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import Root from './Components/Root/Root.jsx';
 import Home from './Components/Home/Home.jsx';
 import Login from './Components/Login/Login.jsx';
 import Register from './Components/Register/Register.jsx';
+import HeroRegister from './Components/HeroRegister/HeroRegister.jsx';
 
 const router = createBrowserRouter([
   {
@@ -27,6 +28,10 @@ const router = createBrowserRouter([
       {
         path:'/register',
         element: <Register></Register>
+      },
+      {
+        path:'/heroRegister',
+        element: <HeroRegister></HeroRegister>
       }
     ]
   },
