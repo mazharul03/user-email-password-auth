41b3f0bf57f621de3fdd263a203d8399e7942571
Mazharul Islam <mazharul3islam0@gmail.com>
2024-11-30 13:10:12 +0600

    added HeroRegister component

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,10 +1,18 @@
 
 const Register = () => {
+
+    const handleRegister = e => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(email, password);
+    }
+
     return (
         <div>
             <div className="mx-auto md:n w-1/2">
                 <h2 className="text-3xl mb-8">Please Register</h2>
-                <form>
+                <form onSubmit={handleRegister}>
                     <input className="mb-4 w-3/4 py-2 px-4" type="email" placeholder="Email Address" name="email" id="" />
                     <br />
                     <input className="mb-4 w-3/4 py-2 px-4" type="password" name="password" placeholder="Password" id="" />
