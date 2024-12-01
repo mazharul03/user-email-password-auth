84ad47d8384979848e0c960e7631a33581038ce9
Mazharul Islam <mazharul3islam0@gmail.com>
2024-12-02 00:55:37 +0600

    Login User and Accept terms and conditions

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,6 +1,24 @@
+import { signInWithEmailAndPassword } from 'firebase/auth';
 import React from 'react';
+import auth from '../../firebase/firebase.config';
 
 const Login = () => {
+
+    const handleLogin = e => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(email, password);
+
+        // add validation
+
+        signInWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                console.log(result.user)
+            })
+            .catch(error => console.error(error))
+    }
+
     return (
         <div className="hero bg-base-200 min-h-screen">
             <div className="hero-content flex-col lg:flex-row-reverse">
@@ -12,7 +30,7 @@ const Login = () => {
                     </p>
                 </div>
                 <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-                    <form className="card-body">
+                    <form onSubmit={handleLogin} className="card-body">
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
